975e6d7f2487500c12fb095ab3290c031f2652d0
Paola Garbato <paolagarbato@gmail.com>
2020-08-19 16:37:30 -0300

    Adicionando codigos

--- Lanchonete/Form1.Designer.cs
+++ Lanchonete/Form1.Designer.cs
@@ -39,9 +39,9 @@
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.pnlCardapio = new System.Windows.Forms.Panel();
             this.label4 = new System.Windows.Forms.Label();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pctCardapio = new System.Windows.Forms.PictureBox();
             this.pictureBox4 = new System.Windows.Forms.PictureBox();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
@@ -50,8 +50,8 @@
             this.label9 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.pnlCardapio.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pctCardapio)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             this.SuspendLayout();
             // 
@@ -75,6 +75,7 @@
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // label2
             // 
@@ -94,6 +95,7 @@
             this.txtProduto.Name = "txtProduto";
             this.txtProduto.Size = new System.Drawing.Size(100, 22);
             this.txtProduto.TabIndex = 3;
+            this.txtProduto.TextChanged += new System.EventHandler(this.txtProduto_TextChanged);
             // 
             // lstProdutos
             // 
@@ -102,6 +104,7 @@
             this.lstProdutos.Name = "lstProdutos";
             this.lstProdutos.Size = new System.Drawing.Size(234, 95);
             this.lstProdutos.TabIndex = 4;
+            this.lstProdutos.SelectedIndexChanged += new System.EventHandler(this.lstProdutos_SelectedIndexChanged);
             // 
             // label3
             // 
@@ -134,6 +137,7 @@
             this.pictureBox2.Size = new System.Drawing.Size(92, 111);
             this.pictureBox2.TabIndex = 7;
             this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
             // 
             // button1
             // 
@@ -145,6 +149,7 @@
             this.button1.TabIndex = 8;
             this.button1.Text = "REMOVER";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -156,22 +161,23 @@
             this.button2.TabIndex = 9;
             this.button2.Text = "LIMPAR";
             this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // panel1
+            // pnlCardapio
             // 
-            this.panel1.BackColor = System.Drawing.Color.White;
-            this.panel1.Controls.Add(this.label9);
-            this.panel1.Controls.Add(this.label8);
-            this.panel1.Controls.Add(this.label7);
-            this.panel1.Controls.Add(this.label6);
-            this.panel1.Controls.Add(this.label5);
-            this.panel1.Controls.Add(this.pictureBox4);
-            this.panel1.Controls.Add(this.label4);
-            this.panel1.Location = new System.Drawing.Point(33, 88);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(253, 255);
-            this.panel1.TabIndex = 10;
-            this.panel1.Visible = false;
+            this.pnlCardapio.BackColor = System.Drawing.Color.White;
+            this.pnlCardapio.Controls.Add(this.label9);
+            this.pnlCardapio.Controls.Add(this.label8);
+            this.pnlCardapio.Controls.Add(this.label7);
+            this.pnlCardapio.Controls.Add(this.label6);
+            this.pnlCardapio.Controls.Add(this.label5);
+            this.pnlCardapio.Controls.Add(this.pictureBox4);
+            this.pnlCardapio.Controls.Add(this.label4);
+            this.pnlCardapio.Location = new System.Drawing.Point(33, 88);
+            this.pnlCardapio.Name = "pnlCardapio";
+            this.pnlCardapio.Size = new System.Drawing.Size(253, 255);
+            this.pnlCardapio.TabIndex = 10;
+            this.pnlCardapio.Visible = false;
             // 
             // label4
             // 
@@ -183,15 +189,16 @@
             this.label4.TabIndex = 0;
             this.label4.Text = "CARDÁPIO";
             // 
-            // pictureBox3
+            // pctCardapio
             // 
-            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(37, 26);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(50, 36);
-            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox3.TabIndex = 11;
-            this.pictureBox3.TabStop = false;
+            this.pctCardapio.Image = ((System.Drawing.Image)(resources.GetObject("pctCardapio.Image")));
+            this.pctCardapio.Location = new System.Drawing.Point(37, 26);
+            this.pctCardapio.Name = "pctCardapio";
+            this.pctCardapio.Size = new System.Drawing.Size(50, 36);
+            this.pctCardapio.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pctCardapio.TabIndex = 11;
+            this.pctCardapio.TabStop = false;
+            this.pctCardapio.Click += new System.EventHandler(this.pctCardapio_Click);
             // 
             // pictureBox4
             // 
@@ -202,6 +209,7 @@
             this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox4.TabIndex = 1;
             this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
             // 
             // label5
             // 
@@ -259,8 +267,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(320, 370);
-            this.Controls.Add(this.pictureBox3);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pctCardapio);
+            this.Controls.Add(this.pnlCardapio);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox2);
@@ -276,9 +284,9 @@
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.pnlCardapio.ResumeLayout(false);
+            this.pnlCardapio.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pctCardapio)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -297,10 +305,10 @@
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel pnlCardapio;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.PictureBox pictureBox4;
-        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pctCardapio;
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label7;
--- Lanchonete/Form1.cs
+++ Lanchonete/Form1.cs
@@ -21,5 +21,94 @@ namespace Lanchonete
         {
 
         }
+
+        private void pctCardapio_Click(object sender, EventArgs e)
+        {
+            pnlCardapio.Visible = true;
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            pnlCardapio.Visible = false;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void txtProduto_TextChanged(object sender, EventArgs e)
+        {
+            double total = 0.00;
+            if (txtProduto.Text.Length == 3)
+            {
+                if (txtProduto.Text == "001")
+                {
+                    lstProdutos.Items.Add(txtProduto.Text + " SALGADO - R$ 4.50");
+                    txtProduto.Text = "";
+                    txtProduto.Focus();
+                    total += 4.50;
+                }
+                else if (txtProduto.Text == "002")
+                {
+                    lstProdutos.Items.Add(txtProduto.Text + " SUCO NATURAL - R$ 3.50");
+                    txtProduto.Text = "";
+                    txtProduto.Focus();
+                    total += 3.50;
+                }
+                else if (txtProduto.Text == "003")
+                {
+                    lstProdutos.Items.Add(txtProduto.Text + " REFRIGERANTE - R$ 4.00");
+                    txtProduto.Text = "";
+                    txtProduto.Focus();
+                    total += 4.00;
+                }
+                else if (txtProduto.Text == "004")
+                {
+                    lstProdutos.Items.Add(txtProduto.Text + " TORTA DOCE - R$ 5.00");
+                    txtProduto.Text = "";
+                    txtProduto.Focus();
+                    total += 5.00;
+                }
+                else if (txtProduto.Text == "005")
+                {
+                    lstProdutos.Items.Add(txtProduto.Text + " PASTEL - R$ 4.50");
+                    txtProduto.Text = "";
+                    txtProduto.Focus();
+                    total += 4.50;
+                }
+                else
+                {
+                    MessageBox.Show("Código inválido. Verifique o cardápio.", "Inválido");
+                }
+            }
+            else if (txtProduto.Text.Length > 3)
+            {
+                MessageBox.Show("Código inválido. Verifique o cardápio.", "Inválido");
+            }
+
+            lblTotal.Text = "R$ " + total.ToString();
+
+        }
+
+        private void lstProdutos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
